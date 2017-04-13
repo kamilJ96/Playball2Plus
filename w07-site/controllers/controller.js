f3fbfac17c415a2e2df00370da4b68f4da34aa17
Christopher Tatli <christopher.tatli@gmail.com>
2017-04-13 10:43:48 +1000

    updated controller.js

--- w07-site/controllers/controller.js
+++ w07-site/controllers/controller.js
@@ -2,38 +2,64 @@
  * Created by Eduardo Velloso on 10/04/2017.
  */
 var mongoose = require('mongoose');
-var Cafe = mongoose.model('Cafe');
+var Album = mongoose.model('Album');
+var Artist = mongoose.model('Artist');
 
-var createCafe = function(req,res){
-    var cafe = new Cafe({
-        "name":req.body.name,
-        "address":req.body.address,
-        "distance":req.body.distance,
-        "rating":req.body.rating,
-        "photo":req.body.photo
-    });
-    cafe.save(function(err,newCafe){
-        if(!err){
-            res.send(newCafe);
-        }else{
-            res.sendStatus(400);
-        }
-    });
+var createAlbum = function(req,res){
+   Artist.findById(req.params.id, function(err, artist) {
+      if(!err){
+
+         artist.albums.push({
+            "year":req.body.year,
+            "title":req.body.title,
+            "track_count":req.body.track_count
+         });
+         artist.save();
+
+      }else{
+          res.sendStatus(404);
+      }
+
+   });
 };
 
-var findAllCafes = function(req,res){
-    Cafe.find(function(err,cafes){
+var createArtist = function(req,res){
+   var artist = new Artist({
+      "name":req.body.name
+   });
+   artist.save(function(err,newArtist){
+      if(!err){
+           res.send(newArtist);
+      }else{
+           res.sendStatus(400);
+      }
+   });
+};
+
+var findAllArtists = function(req,res){
+    Artist.find(function(err,artists){
         if(!err){
-            res.send(cafes);
+            res.send(artists);
         }else{
             res.sendStatus(404);
         }
     });
 };
 
+var findAllAlbums = function(req,res){
+    Album.find(function(err,albums){
+        if(!err){
+            res.send(albums);
+        }else{
+            res.sendStatus(404);
+        }
+    });
+};
+
+
 var findOneCafe = function(req,res){
     var cafeInx = req.params.id;
-    Cafe.findById(cafeInx,function(err,cafe){
+    Artist.findById(cafeInx,function(err,cafe){
         if(!err){
             res.send(cafe);
         }else{

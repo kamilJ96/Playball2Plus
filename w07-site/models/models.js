e89ed605c39d9273caf86766fdbc9e2cbdf8c67d
Matt Dunwoodie <mdunwoodie@student.unimelb.edu.au>
2017-04-13 10:23:48 +1000

    Update models

--- w07-site/models/models.js
+++ w07-site/models/models.js
@@ -1,12 +1,14 @@
 /**
  * Created by Eduardo Velloso on 10/04/2017.
  */
+
 var mongoose = require('mongoose');
+var ObjectId = mongoose.Schema.ObjectId;
 var Album = new mongoose.Schema({
     album       : ObjectId
-  , year        : int
+  , year        : Number
   , title       : String
-  , track_count : int
+  , track_count : Number
 });
 var Artist = new mongoose.Schema({
     name      : String

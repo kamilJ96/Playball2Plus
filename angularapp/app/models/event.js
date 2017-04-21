f1fa64052e8bc0bcf62abe7fa71ed51216add933
Kam <kjakrzewski@student.unimelb.edu.au>
2017-04-21 14:41:40 +1000

    added description

--- angularapp/app/models/event.js
+++ angularapp/app/models/event.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var eventSchema = mongoose.Schema(
   {
     'name': 			String,
+    'description':		String,
     'address': 			String,
     'participantsRequired':	Number,
     'sport':			String,
